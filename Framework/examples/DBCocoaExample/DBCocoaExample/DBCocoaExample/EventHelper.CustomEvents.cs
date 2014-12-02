548803830f69e09c8eba7f2a9392f66daa4e078a
Jonathan Mitchell <jonathan@thesaurussoftware.com>
2014-12-02 21:40:14 +0000

    Added usage comments to EventHelper partial.

--- Framework/examples/DBCocoaExample/DBCocoaExample/DBCocoaExample/EventHelper.CustomEvents.cs
+++ Framework/examples/DBCocoaExample/DBCocoaExample/DBCocoaExample/EventHelper.CustomEvents.cs
@@ -7,6 +7,12 @@ namespace Dubrovnik.ClientApplication
     public partial class EventHelper
     {
 
+        /*
+         * In order to rout managed events back into unmanaged code it is necessary to define
+         * a static call back handler that is registered to a static function in unmanaged code.
+         */
+        //[MethodImpl(MethodImplOptions.InternalCall)]
+        //public static extern void CustomEventName(object sender, EventArgs args);
     }
 
 }
