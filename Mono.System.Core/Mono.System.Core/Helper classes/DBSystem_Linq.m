a0795daf865c90208cfa4608da2b6d7821acc860
Jonathan Mitchell <jonathan@mugginsoft.com>
2015-03-06 10:13:59 +0000

    Generic interfaces may in some cases be backed by System.Array objects. Raise an exception if this situation is detected when trying to generate an List<T>

--- Mono.System.Core/Mono.System.Core/Helper classes/DBSystem_Linq.m
+++ Mono.System.Core/Mono.System.Core/Helper classes/DBSystem_Linq.m
@@ -14,12 +14,28 @@
 
 @implementation DBSystem_Linq
 
-+ (DBSystem_Collections_IList *)toList:(DBManagedObject <Interface_IEnumerable_T> *)managedObject
++ (DBSystem_Collections_IList *)toList:(System_Object <Interface_IEnumerable_T> *)managedObject
 {
     return [self toList:managedObject genericTypeIndex:0];
 }
 
-+ (DBSystem_Collections_IList *)toList:(DBManagedObject <Interface_IEnumerable_T> *)managedObject genericTypeIndex:(NSUInteger)typeIndex
++ (DBSystem_Collections_IList *)toList:(System_Object <Interface_IEnumerable_T> *)managedObject genericTypeIndex:(NSUInteger)typeIndex
+{
+
+    if (!managedObject.db_getType.isGenericType) {
+        NSException *e = [NSException exceptionWithName:@"DBSystemLinqToList" reason:[NSString stringWithFormat:@"This method requires a generic type : %@. This exception may be encountered when accessing arrays via an IEnumerableT interface", [managedObject.db_getType toString]] userInfo:nil];
+        
+        [e raise];
+    }
+    
+    // Get the type with which to inflate the method.
+    // The generic type for the method equals the generic type of this object at the given index
+    MonoType *genericMonoType = [managedObject getMonoGenericTypeAtIndex:typeIndex];
+    
+    return [self toList:managedObject monoType:genericMonoType];
+}
+
++ (DBSystem_Collections_IList *)toList:(System_Object <Interface_IEnumerable_T> *)managedObject monoType:(MonoType *)monoType
 {
     // NOTE: perhaps a C# helper method could achieve this more simply.
     
@@ -28,14 +44,13 @@
     // note that the generic parameter type must be obtained from the source.
     // the method obtained here cannot be called directly but must be inflated with a type (see below)
     DBManagedMethod *managedMethod = [DBManagedMethod
-                           methodWithMonoMethodNamed:"ToList(System.Collections.Generic.IEnumerable`1<TSource>)"
-                           className:"System.Linq.Enumerable"
-                           assemblyName:"System.Core"];
-    
+                                      methodWithMonoMethodNamed:"ToList(System.Collections.Generic.IEnumerable`1<TSource>)"
+                                      className:"System.Linq.Enumerable"
+                                      assemblyName:"System.Core"];
+
     // Get the type with which to inflate the method.
-    // The generic type for the method equals the generic type of this object at the given index
-    managedMethod.genericMonoType = [managedObject getMonoGenericTypeAtIndex:typeIndex];
-        
+    managedMethod.genericMonoType = monoType;
+    
     // Invoke the extension method passing mono object as first argument
     // NOTE: we could invoke this as a class method but it is clean to invoke against self.
     MonoObject *monoListObject = [managedObject invokeMethod:managedMethod withNumArgs:1, [managedObject monoObject]];
@@ -46,12 +61,12 @@
     return list;
 }
 
-+ (NSMutableArray *)toMutableArray:(DBManagedObject <Interface_IEnumerable_T> *)managedObject
++ (NSMutableArray *)toMutableArray:(System_Object <Interface_IEnumerable_T> *)managedObject
 {
     return [[self toList:managedObject] mutableArray];
 }
 
-+ (NSArray *)toArray:(DBManagedObject <Interface_IEnumerable_T> *)managedObject
++ (NSArray *)toArray:(System_Object <Interface_IEnumerable_T> *)managedObject
 {
     return [[self toList:managedObject] array];
 }
