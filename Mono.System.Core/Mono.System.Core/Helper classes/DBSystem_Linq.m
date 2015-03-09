2e4126e339c46edc74d74eb7b85391db4911f2af
Jonathan Mitchell <jonathan@mugginsoft.com>
2015-03-09 12:01:32 +0000

    Fix System_Linq toList: crash with nil managed object argument.

--- Mono.System.Core/Mono.System.Core/Helper classes/DBSystem_Linq.m
+++ Mono.System.Core/Mono.System.Core/Helper classes/DBSystem_Linq.m
@@ -21,6 +21,10 @@
 
 + (DBSystem_Collections_IList *)toList:(System_Object <Interface_IEnumerable_T> *)managedObject genericTypeIndex:(NSUInteger)typeIndex
 {
+    if (!managedObject) {
+        return nil;
+    }
+    
     // TODO System.Array can implement IList<T> in some cases
     // see https://msdn.microsoft.com/en-us/library/ms228502.aspx
     // for now we raise
