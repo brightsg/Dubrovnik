a9d8efbe3310ad879ed62c9db665a1270ed93f04
Jonathan Mitchell <jonathan@thesaurussoftware.com>
2016-02-17 13:17:41 +0000

    Added XsltArgumentList stub

---
+++ Mono.System.Xml/Mono.System.Xml/Manual Bindings/System_Xml_Xsl_XsltArgumentList.h
@@ -0,0 +1,11 @@
+//
+//  System_Xml_Xsl_XsltArgumentList.h
+//  Mono.System.Xml
+//
+//  Created by Jonathan Mitchell on 16/02/2016.
+//  Copyright © 2016 Thesaurus Software. All rights reserved.
+//
+
+@interface System_Xml_Xsl_XsltArgumentList : System_Object
+
+@end
