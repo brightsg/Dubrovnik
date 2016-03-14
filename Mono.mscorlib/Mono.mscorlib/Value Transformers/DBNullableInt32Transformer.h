bf4ba2b0d53696fa17ad4d2a303fe04052fa8e66
Jonathan Mitchell <jonathan@thesaurussoftware.com>
2016-03-14 12:06:38 +0000

    Added nullable int32 transformer.

---
+++ Mono.mscorlib/Mono.mscorlib/Value Transformers/DBNullableInt32Transformer.h
@@ -0,0 +1,12 @@
+//
+//  DBNullableInt32Transformer.h
+//  Mono.mscorlib
+//
+//  Created by Jonathan Mitchell on 14/03/2016.
+//  Copyright © 2016 Thesaurus Software. All rights reserved.
+//
+#import <Foundation/Foundation.h>
+
+@interface DBNullableInt32Transformer : NSValueTransformer
+
+@end
---
+++ Mono.mscorlib/Mono.mscorlib/Value Transformers/DBNullableInt32Transformer.m
@@ -0,0 +1,39 @@
+//
+//  DBNullableInt32Transformer.m
+//  Mono.mscorlib
+//
+//  Created by Jonathan Mitchell on 14/03/2016.
+//  Copyright © 2016 Thesaurus Software. All rights reserved.
+//
+
+#import "DBNullableInt32Transformer.h"
+#import "System_NullableA1.h"
+
+@implementation DBNullableInt32Transformer
+
++ (Class)transformedValueClass
+{
+    return [NSNumber class];
+}
+
++ (BOOL)allowsReverseTransformation
+{
+    return YES;
+}
+
+- (id)transformedValue:(id)value
+{
+    return [value numberValue];
+}
+
+- (id)reverseTransformedValue:(id)value
+{
+    if (!value) {
+        value = [NSNumber numberWithInteger:0];
+    }
+    System_NullableA1 *nullableA1 = [System_NullableA1 newNullableFromObject:value withTypeArgumentName:@"int32_t"];
+    
+    return nullableA1;
+}
+
+@end
