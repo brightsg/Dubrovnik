1a12d3d36d8ed2d5c3f629ead6b7f6854f93df93
Jonathan Mitchell <jonathan@thesaurussoftware.com>
2016-09-27 09:51:49 +0100

    Managed enumerations class represntation.

--- dotNET/UnitTests/GeneratedObjC/Dubrovnik_UnitTests.h
+++ dotNET/UnitTests/GeneratedObjC/Dubrovnik_UnitTests.h
@@ -9,10 +9,10 @@
 // http://github.com/ThesaurusSoftware/Dubrovnik
 // This code has dependencies on the above project.
 //
-// Date:     26/09/2016 13:30:22
+// Date:     27/09/2016 09:44:55
 //
 // Assembly: Dubrovnik.UnitTests
-// Fullname: Dubrovnik.UnitTests, Version=1.0.6113.22509, Culture=neutral, PublicKeyToken=null
+// Fullname: Dubrovnik.UnitTests, Version=1.0.6114.15745, Culture=neutral, PublicKeyToken=null
 // Path:     C:\Users\Jonathan\Documents\Dubrovnik\dotNET\UnitTests\Dubrovnik.UnitTests\bin\Debug\Dubrovnik.UnitTests.exe
 //
 // Platform: Microsoft Windows NT 6.2.9200.0
@@ -217,6 +217,20 @@
 #warning DUReferenceStruct_ class name collision.
 #else
 #define DUReferenceStruct_ Dubrovnik_UnitTests_ReferenceStruct
+#endif
+
+@class Dubrovnik_UnitTests_IntEnum;
+#ifdef DUIntEnum_
+#warning DUIntEnum_ class name collision.
+#else
+#define DUIntEnum_ Dubrovnik_UnitTests_IntEnum
+#endif
+
+@class Dubrovnik_UnitTests_LongEnum;
+#ifdef DULongEnum_
+#warning DULongEnum_ class name collision.
+#else
+#define DULongEnum_ Dubrovnik_UnitTests_LongEnum
 #endif
 
 @class Dubrovnik_UnitTests_Extensions_ReferenceObjectExtensions;
