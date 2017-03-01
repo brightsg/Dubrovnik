ac11a5818beb529cf339051dac0589e7cecfff3e
Richard Webb <webby@beardmouse.org.uk>
2017-03-01 15:43:37 +0000

    extend the IEnumerable bindings, and add the getEnumerator() functions

--- Mono.mscorlib/Mono.mscorlib/Manual bindings/System_Collections_IEnumerable.h
+++ Mono.mscorlib/Mono.mscorlib/Manual bindings/System_Collections_IEnumerable.h
@@ -6,6 +6,53 @@
 //
 //
 
+@protocol System_Collections_IEnumerator;
+
+
+//
+// Adoption protocol
+//
+@protocol System_Collections_IEnumerable_ <System_Object_>
+
+@end
+
+
+//
+// Implementation protocol
+//
+@protocol System_Collections_IEnumerable <System_Collections_IEnumerable_, System_Object>
+
+@optional
+
+#pragma mark -
+#pragma mark Methods
+
+// Managed method name : GetEnumerator
+// Managed return type : System.Collections.IEnumerator
+// Managed param types :
+- (id <System_Collections_IEnumerator>)getEnumerator;
+
+@end
+
+
+//
+// Interface
+//
 @interface System_Collections_IEnumerable : System_Object
+
+#pragma mark -
+#pragma mark Setup
+// obligatory override
++ (const char *)monoClassName;
+// obligatory override
++ (const char *)monoAssemblyName;
+
+#pragma mark -
+#pragma mark Methods
+
+// Managed method name : GetEnumerator
+// Managed return type : System.Collections.IEnumerator
+// Managed param types :
+- (id <System_Collections_IEnumerator>)getEnumerator;
 
 @end
