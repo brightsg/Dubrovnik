7298e86551d42d2eb84c746315323f8a13a7fdc1
Jonathan Mitchell <jonathan@mugginsoft.com>
2017-03-23 10:34:56 +0000

    Completes SystemDayOfWeek

--- Mono.mscorlib/Mono.mscorlib/Manual bindings/System_DayOfWeek.h
+++ Mono.mscorlib/Mono.mscorlib/Manual bindings/System_DayOfWeek.h
@@ -10,5 +10,11 @@
 // Mono enumeration : System.DayOfWeek
 //
 typedef NS_ENUM(int32_t, System_DayOfWeek) {
-	System_DayOfWeek_Monday = 1,
+    System_DayOfWeek_Friday = 5,
+    System_DayOfWeek_Monday = 1,
+    System_DayOfWeek_Saturday = 6,
+    System_DayOfWeek_Sunday = 0,
+    System_DayOfWeek_Thursday = 4,
+    System_DayOfWeek_Tuesday = 2,
+    System_DayOfWeek_Wednesday = 3,
 };
