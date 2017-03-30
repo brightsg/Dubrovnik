f4c4ec1aede5544def9dff193ac38b7ed6e19eb8
Jonathan Mitchell <jonathan@mugginsoft.com>
2017-03-30 10:00:59 +0100

    Fixes assembly loading search.

--- Mono.System.Xml/Mono.System.Xml/Manual Bindings/System_Xml_Xsl_XsltArgumentList.h
+++ Mono.System.Xml/Mono.System.Xml/Manual Bindings/System_Xml_Xsl_XsltArgumentList.h
@@ -1,11 +1,52 @@
+﻿//++Dubrovnik.CodeGenerator System_Xml_Xsl_XsltArgumentList.h
 //
-//  System_Xml_Xsl_XsltArgumentList.h
-//  Mono.System.Xml
+// Managed class : XsltArgumentList
 //
-//  Created by Jonathan Mitchell on 16/02/2016.
-//  Copyright © 2016 Thesaurus Software. All rights reserved.
-//
-
 @interface System_Xml_Xsl_XsltArgumentList : System_Object
 
+#pragma mark -
+#pragma mark Setup
+	// obligatory override
+	+ (const char *)monoClassName;
+	// obligatory override
+	+ (const char *)monoAssemblyName;
+
+#pragma mark -
+#pragma mark Methods
+
+	// Managed method name : AddExtensionObject
+	// Managed return type : System.Void
+	// Managed param types : System.String, System.Object
+    - (void)addExtensionObject_withNamespaceUri:(NSString *)p1 extension:(System_Object *)p2;
+
+	// Managed method name : AddParam
+	// Managed return type : System.Void
+	// Managed param types : System.String, System.String, System.Object
+    - (void)addParam_withName:(NSString *)p1 namespaceUri:(NSString *)p2 parameter:(System_Object *)p3;
+
+	// Managed method name : Clear
+	// Managed return type : System.Void
+	// Managed param types : 
+    - (void)clear;
+
+	// Managed method name : GetExtensionObject
+	// Managed return type : System.Object
+	// Managed param types : System.String
+    - (System_Object *)getExtensionObject_withNamespaceUri:(NSString *)p1;
+
+	// Managed method name : GetParam
+	// Managed return type : System.Object
+	// Managed param types : System.String, System.String
+    - (System_Object *)getParam_withName:(NSString *)p1 namespaceUri:(NSString *)p2;
+
+	// Managed method name : RemoveExtensionObject
+	// Managed return type : System.Object
+	// Managed param types : System.String
+    - (System_Object *)removeExtensionObject_withNamespaceUri:(NSString *)p1;
+
+	// Managed method name : RemoveParam
+	// Managed return type : System.Object
+	// Managed param types : System.String, System.String
+    - (System_Object *)removeParam_withName:(NSString *)p1 namespaceUri:(NSString *)p2;
 @end
+//--Dubrovnik.CodeGenerator
