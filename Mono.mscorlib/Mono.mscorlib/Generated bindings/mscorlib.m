3d7d515fbea9fe27a7e88c73628e2112eb9847ea
Jonathan Mitchell <jonathan@thesaurussoftware.com>
2018-03-08 10:44:11 +0000

    mscorlib regen just to confirm that the previous merge was solid.

--- Mono.mscorlib/Mono.mscorlib/Generated bindings/mscorlib.m
+++ Mono.mscorlib/Mono.mscorlib/Generated bindings/mscorlib.m
@@ -9,7 +9,7 @@
 // http://github.com/ThesaurusSoftware/Dubrovnik
 // This code has dependencies on the above project.
 //
-// Date:     04/03/2018 22:35:42
+// Date:     08/03/2018 10:40:19
 //
 // Assembly: mscorlib
 // Fullname: mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089
