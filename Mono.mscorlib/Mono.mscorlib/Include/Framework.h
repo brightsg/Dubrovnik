f751dc08334cb6e812bf479a042044117b681859
Jonathan Mitchell <jonathan@mugginsoft.com>
2018-03-09 15:03:43 +0000

    Remove legacy predeclarations from mscorlib header.

--- Mono.mscorlib/Mono.mscorlib/Include/Framework.h
+++ Mono.mscorlib/Mono.mscorlib/Include/Framework.h
@@ -8,32 +8,7 @@
 
 #import <Foundation/Foundation.h>
 
-@class System_Object;
-@class System_Type;
-
-//
-// Adoption protocol
-//
-@protocol System_Object_ <NSObject, DBManagedObject>
-@end
-
-//
-// Implementation protocol
-//
-@protocol System_Object <System_Object_>
-
-@required
-- (BOOL)equals_withObj:(System_Object *)p1;
-- (BOOL)equals_withObjA:(System_Object *)p1 objB:(System_Object *)p2;
-- (int32_t)db_getHashCode;
-- (System_Type *)db_getType;
-- (BOOL)referenceEquals_withObjA:(System_Object *)p1 objB:(System_Object *)p2;
-- (NSString *)toString;
-
-@end
-
 #import "mscorlib.h"
 #import "Categories.h"
 #import "HelperClasses.h"
 #import "ValueTransformers.h"
-
