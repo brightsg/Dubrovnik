8790948f2c90755c9ab6ae799957772465165307
Jonathan Mitchell <ultimate-task@btconnect.com>
2019-07-04 18:30:44 +0100

    Tidy DBManaged class. This class doesn't get much usage and provides limited facilities. There could be an argument for getting rid of it.

--- Framework/XCode/Representations/DBManagedClass.m
+++ Framework/XCode/Representations/DBManagedClass.m
@@ -17,22 +17,26 @@
 
 @implementation DBManagedClass
 
-+ (instancetype)classWithMonoClass:(MonoClass *)monoClass {
++ (instancetype)classWithMonoClass:(MonoClass *)monoClass
+{
 	DBManagedClass *classRep = [[DBManagedClass alloc] initWithMonoClass:monoClass];
 	return(classRep);
 }
 
-+ (instancetype)classWithMonoClassNamed:(const char *)className fromMonoAssembly:(MonoAssembly *)assembly {
++ (instancetype)classWithMonoClassNamed:(const char *)className fromMonoAssembly:(MonoAssembly *)assembly
+{
 	DBManagedClass *classRep = [[DBManagedClass alloc] initWithMonoClassNamed:className fromMonoAssembly:assembly];
 	return(classRep);
 }
 
-+ (instancetype)classWithMonoClassNamed:(const char *)className {
++ (instancetype)classWithMonoClassNamed:(const char *)className
+{
 	DBManagedClass *classRep = [[DBManagedClass alloc] initWithMonoClassNamed:className];
 	return(classRep);	
 }
 
-- (id)initWithMonoClass:(MonoClass *)klass {
+- (id)initWithMonoClass:(MonoClass *)klass
+{
 	self = [super init];
 
 	if(self) {
@@ -42,20 +46,23 @@
 	return self;
 }
 
-- (id)initWithMonoClassNamed:(const char *)className fromMonoAssembly:(MonoAssembly *)assembly {
+- (id)initWithMonoClassNamed:(const char *)className fromMonoAssembly:(MonoAssembly *)assembly
+{
 	MonoType *type = mono_reflection_type_from_name((char *)className, (MonoImage *)mono_assembly_get_image(assembly));
     NSAssert(type, @"Mono type %s not found in assembly ", className);
 	return [self initWithMonoClass:mono_class_from_mono_type(type)];
 }
 
-- (id)initWithMonoClassNamed:(const char *)className {
+- (id)initWithMonoClassNamed:(const char *)className
+{
 	MonoType *type = mono_reflection_type_from_name((char *)className, mono_get_corlib());
     NSAssert(type, @"Mono type %s not found in corlib", className);
     MonoClass *klass = mono_class_from_mono_type(type);
 	return [self initWithMonoClass:klass];
 }
 
-- (MonoObject *)constructMonoObjectWithNumArgs:(int)numArgs, ... {
+- (MonoObject *)constructMonoObjectWithNumArgs:(int)numArgs, ...
+{
 	va_list va_args;
 	va_start(va_args, numArgs);
 		
@@ -65,17 +72,20 @@
 	return(newObject);
 }
 
-- (MonoClass *)monoClass {
+- (MonoClass *)monoClass
+{
 	return _klass;
 }
 
-- (MonoType *)monoType {
+- (MonoType *)monoType
+{
 	return mono_class_get_type([self monoClass]);
 }
 
 #pragma mark Method Invocation
 
-- (MonoObject *)invokeMonoMethod:(const char *)methodName withNumArgs:(int)numArgs varArgList:(va_list)va_args {
+- (MonoObject *)invokeMonoMethod:(const char *)methodName withNumArgs:(int)numArgs varArgList:(va_list)va_args
+{
 	return(DBMonoClassInvoke(_klass, methodName, numArgs, va_args));
 }
 
@@ -110,7 +120,7 @@
 
 - (MonoObject *)getMonoProperty:(const char *)propertyName
 {
-	return(DBMonoClassGetProperty(_klass, propertyName));
+	return DBMonoClassGetProperty(_klass, propertyName);
 }
 
 - (void)setMonoProperty:(const char *)propertyName value:(void *)value
